55fb6d8c7d7d5b015474edd762da5f05fc182a95
zihao-il <41977021+zihao-il@users.noreply.github.com>
2025-06-03 18:06:34 +0800

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,22 +4,40 @@ import giscusTalk from 'vitepress-plugin-comment-with-giscus';
 import {useData, useRoute} from 'vitepress';
 import {onMounted, watch} from 'vue';
 import './styles.css'
+import '@bprogress/core/css';
+import {BProgress} from '@bprogress/core';
+import mediumZoom from 'medium-zoom';
 
 export default {
     extends: DefaultTheme,
 
     setup() {
-
+        BProgress.configure({
+            showSpinner: true
+        });
         const {frontmatter, isDark} = useData();
         const route = useRoute();
+        watch(
+            () => route.path,
+            (newPath, oldPath) => {
+                BProgress.start()
+                requestAnimationFrame(() => {
+                    BProgress.done()
+                    initZoom()
+                })
+            }
+        );
         const updateThemeColor = (dark: boolean) => {
             const meta = document.querySelector('meta[name="theme-color"]');
             if (meta) {
                 meta.setAttribute('content', dark ? '#1B1B1F' : '#FFFFFF');
             }
         };
-
+        const initZoom = () => {
+            mediumZoom('.main img', {background: 'var(--vp-c-bg)'});
+        };
         onMounted(() => {
+            initZoom();
             updateThemeColor(isDark.value);
             watch(isDark, (newVal) => {
                 updateThemeColor(newVal);
